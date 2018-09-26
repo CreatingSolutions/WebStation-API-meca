93973f8f4ca61556815c9ff05a83f2ff2db6e556
De Baerdemaeker Mathieu <debaerdemaekem@b13p23.fil.univ-lille1.fr>
2018-09-26 10:46:39 +0200

    chore: init unit test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,17 @@
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ExampleTest {
+
+    @Test
+    public void Example() {
+        Example example = new Example();
+
+        Assert.assertEquals("Hello World!", example.home());
+    }
+
+
+}
